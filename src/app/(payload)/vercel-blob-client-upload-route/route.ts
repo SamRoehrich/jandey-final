e28486352ff03613090f3b2f524703fd55a686be
SamRoehrich <sam_roehrich@icloud.com>
2025-12-11 21:17:36 -0700

    chore: try blob again ag

--- src/app/(payload)/vercel-blob-client-upload-route/route.ts
+++
@@ -1,9 +0,0 @@
-import { getClientUploadRoute } from '@payloadcms/storage-vercel-blob'
-
-export const POST = getClientUploadRoute({
-  token: process.env.BLOB_READ_WRITE_TOKEN || '',
-  access: async ({ req }) => {
-    // Only allow authenticated users to upload
-    return !!req.user
-  },
-})
